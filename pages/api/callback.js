484aafdb3c0d22cc4025e8f5d0e55ff00101274e
Tony Martinez <ajosephmartinez@gmail.com>
2020-06-06 21:08:23 -0700

    .

--- pages/api/callback.js
+++ pages/api/callback.js
@@ -2,8 +2,11 @@ import auth0 from "../../lib/auth0";
 
 export default async function callback(req, res) {
   try {
+    console.log("try");
+    debugger;
     await auth0.handleCallback(req, res, { redirectTo: "/" });
   } catch (error) {
+    console.log("catch");
     console.error(error);
     res.status(error.status || 500).end(error.message);
   }
