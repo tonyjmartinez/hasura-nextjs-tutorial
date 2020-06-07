269c27f0dff9792591db83ef9136940fca6244f7
Tony Martinez <ajosephmartinez@gmail.com>
2020-06-07 01:41:55 -0700

    .

--- pages/api/session.js
+++ pages/api/session.js
@@ -1,17 +1,19 @@
-import fetch from 'isomorphic-unfetch';
+import fetch from "isomorphic-unfetch";
 
-import auth0 from '../../lib/auth0';
+import auth0 from "../../lib/auth0";
 
 export default async function session(req, res) {
   try {
+    console.log("session.js");
     const tokenCache = auth0.tokenCache(req, res);
     const { accessToken } = await tokenCache.getAccessToken();
+    console.log("access token? ", accessToken, tokenCache);
     res.status(200).json({ accessToken });
   } catch (error) {
     console.error(error);
     res.status(error.status || 500).json({
       code: error.code,
-      error: error.message
+      error: error.message,
     });
   }
 }
