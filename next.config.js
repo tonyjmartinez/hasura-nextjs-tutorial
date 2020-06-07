c7f1cdc3aa579c83870450badf4164ad53962a9e
Tony Martinez <ajosephmartinez@gmail.com>
2020-06-06 18:35:30 -0700

    .

--- next.config.js
+++ next.config.js
@@ -1,5 +1,5 @@
-const dotenv = require('dotenv')
-dotenv.config()
+const dotenv = require("dotenv");
+dotenv.config();
 
 module.exports = {
   env: {
@@ -7,13 +7,13 @@ module.exports = {
     AUTH0_AUDIENCE: process.env.AUTH0_AUDIENCE,
     AUTH0_CLIENT_ID: process.env.AUTH0_CLIENT_ID,
     AUTH0_CLIENT_SECRET: process.env.AUTH0_CLIENT_SECRET,
-    AUTH0_SCOPE: 'openid profile',
+    AUTH0_SCOPE: "openid profile",
     REDIRECT_URI:
-      process.env.REDIRECT_URI || 'http://localhost:3000/api/callback',
+      process.env.REDIRECT_URI || "http://localhost:3000/api/callback",
     POST_LOGOUT_REDIRECT_URI:
-      process.env.POST_LOGOUT_REDIRECT_URI || 'http://localhost:3000/',
+      process.env.POST_LOGOUT_REDIRECT_URI || "http://localhost:3000/",
     SESSION_COOKIE_SECRET: process.env.SESSION_COOKIE_SECRET,
     SESSION_COOKIE_LIFETIME: 7200, // 2 hours
     APP_HOST: process.env.DOMAIN,
   },
-}
+};
