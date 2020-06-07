4e25a9aca3e7a9cdcff07d571f70e7fc8fb2af7c
Tony Martinez <ajosephmartinez@gmail.com>
2020-06-07 01:01:32 -0700

    .

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ dotenv.config();
 
 module.exports = {
   env: {
-    AUTH0_DOMAIN: process.env.AUTH0_DOMAIN,
+    AUTH0_DOMAIN: "tonyjmartinez.auth0.com",
     AUTH0_AUDIENCE: process.env.AUTH0_AUDIENCE,
     AUTH0_CLIENT_ID: process.env.AUTH0_CLIENT_ID,
     AUTH0_CLIENT_SECRET: process.env.AUTH0_CLIENT_SECRET,
