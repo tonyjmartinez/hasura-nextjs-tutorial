b7dfa7a7739964e4ce4f774ff144fedaf299a67c
Tony Martinez <ajosephmartinez@gmail.com>
2020-06-07 01:51:10 -0700

    .

--- pages/api/login.js
+++ pages/api/login.js
@@ -2,14 +2,11 @@ import auth0 from "../../lib/auth0";
 import config from "../../lib/auth0-config";
 
 export default async function login(req, res) {
-  console.log("login called");
   try {
-    console.log("here...", config.AUTH0_DOMAIN);
     debugger;
     await auth0.handleLogin(req, res);
   } catch (error) {
     console.error(error);
-    console.log("something");
     res.status(error.status || 500).end(error.message);
   }
 }
